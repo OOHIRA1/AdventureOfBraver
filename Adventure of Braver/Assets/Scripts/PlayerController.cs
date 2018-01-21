d5767b9ba5b0088b70962d6590cc8783807bf33b
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-01-22 01:21:31 +0900

    アドブレのデータの移行１
    ・アドブレのデータをBitBucketからGitHubに移行しました（途中）

--- Adventure of Braver/Assets/Scripts/PlayerController.cs
+++ Adventure of Braver/Assets/Scripts/PlayerController.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
 
     [SerializeField]
-    float walkSpeed, runSpeed;
+    float walkSpeed, runSpeed, jumpHeight, strafeSpeed;
+
+    [Range(0, 1)]
+    public float airControlPercent;
 
     [SerializeField]
     float turnSmoothTime = 0.2f;
@@ -15,36 +18,112 @@ public class PlayerController : MonoBehaviour {
     float speedSmoothTime = 0.1f;
     float speedSmoothVelocity;
     float currentSpeed;
+    float velocityY;
 
+    Transform myTransform;
     Animator animator;
     Transform cameraT;
+    CharacterController controller;
+    Targeting targeting;
 
-	// Use this for initialization
-	void Start () {
+    public float gravity = -12;
+
+    // Use this for initialization
+    void Start () {
+        targeting = GetComponent<Targeting>();
         animator = GetComponent<Animator>();
         cameraT = Camera.main.transform;
+        controller = GetComponent<CharacterController>();
+        myTransform = transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+        //インプット
         Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         Vector2 inputDir = input.normalized;
 
+        bool running = Input.GetKey(KeyCode.LeftShift);
+        Move(inputDir, running);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        if (targeting.isTargeting)
+        {
+            Strafe();
+        }
+
+        //モーション
+        float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * .5f);
+        animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+
+    }
+
+    void Move( Vector2 inputDir, bool running)
+    {
         if (inputDir != Vector2.zero)
         {
             float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
-            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSmoothTime);
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
         }
 
-        bool running = Input.GetKey(KeyCode.LeftShift);
+        
         float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
-        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
 
-        transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+        velocityY += Time.deltaTime * gravity;
 
-        float animationSpeedPercent = ((running) ? 1 : .5f) * inputDir.magnitude;
-        animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+        Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+        controller.Move(velocity * Time.deltaTime);
+        currentSpeed = new Vector2(controller.velocity.x, controller.velocity.z).magnitude;
 
-	}
+        if (controller.isGrounded)
+        {
+            velocityY = 0;
+        }
+
+        
+    }
+
+    void Jump()
+    {
+        if(controller.isGrounded)
+        {
+            float jumpVelocity = Mathf.Sqrt(-2 * gravity * jumpHeight);
+            velocityY = jumpVelocity;
+        }
+    }
+
+    float GetModifiedSmoothTime( float smoothTime)
+    {
+        if( controller.isGrounded)
+        {
+            return smoothTime;
+        }
+
+        if ( airControlPercent == 0)
+        {
+            return float.MaxValue;
+        }
+
+        return smoothTime / airControlPercent;
+    }
+
+    void Strafe()
+    {
+        if ((Input.GetAxis("Horizontal")) > 0)
+        {
+            //anim here
+            controller.SimpleMove(myTransform.TransformDirection(Vector3.right) * Input.GetAxis("Horizontal") * strafeSpeed);
+        }
+        else if ((Input.GetAxis("Horizontal")) < 0)
+        {
+            //anim here
+            controller.SimpleMove(myTransform.TransformDirection(Vector3.right) * Input.GetAxis("Horizontal") * strafeSpeed);
+        }
+    }
 }
