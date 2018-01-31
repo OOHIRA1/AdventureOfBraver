c589b91665b657571bea8f80579e5537110d676a
suslovd <suslovdd@gmail.com>
2018-01-31 19:23:41 +0900

    + 会話スクリプト
    + 様々なキャラモデル

---
+++ Adventure of Braver/Assets/Scripts/CitizenAnimator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CitizenAnimator : MonoBehaviour
+{
+
+    const float locomotionAnimationSmootTime = .05f;
+
+    NavMeshAgent agent;
+    Animator anim;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedPercent = agent.velocity.magnitude / agent.speed;
+        anim.SetFloat("speedPercent", speedPercent, locomotionAnimationSmootTime, Time.deltaTime);
+    }
+}
---
+++ Adventure of Braver/Assets/Scripts/PlayerScripts/DialogueTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour {
+
+    public Dialogue dialogue;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if( other.tag == "Player")
+        {
+            TriggerDialogue();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if( other.tag == "Player")
+        {
+            StopDialogue();
+        }
+    }
+
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    public void StopDialogue()
+    {
+        FindObjectOfType<DialogueManager>().EndDialogue();
+    }
+}
