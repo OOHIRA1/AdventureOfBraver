c519721e6538ff7e38c02b4dcd568f23ca670755
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-05-03 15:31:28 +0900

    プレイヤーのアニメーションの編集
    ・攻撃待機アニメーションなどを違和感なく遷移するように設定しました
    ・各アニメーションに遷移させる関数を作成しました

--- Adventure of Braver/Assets/Scripts/PlayerScripts/PlayerAnimationControll.cs
+++ Adventure of Braver/Assets/Scripts/PlayerScripts/PlayerAnimationControll.cs
@@ -8,7 +8,7 @@ using UnityEngine;
 [RequireComponent(typeof(Animator))]
 public class PlayerAnimationControll : MonoBehaviour {
 	const float LOCOMOTION_ANIMATION_SMOOTH_TIME = 0.5f;
-	const float ATTAACK_INTERVAL = 1f;
+	const float ATTACK_INTERVAL = 1f;
 
 	Animator _animator;
 	float _attackTime;
@@ -22,16 +22,23 @@ public class PlayerAnimationControll : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.F))
-			_animator.SetTrigger ("Attack");
+			AttackSingle ();
 		if (Input.GetKeyDown (KeyCode.C))
-			_animator.SetTrigger ("attackDouble");
+			AttackDouble ();
 		if (Input.GetKeyDown (KeyCode.D))
-			_animator.SetTrigger ("death");
+			Death ();
 		if (Input.GetKeyDown (KeyCode.J))
 			_animator.SetTrigger ("skillJumpAttack");
 		if (Input.GetKeyDown (KeyCode.H))
-			_animator.SetTrigger ("skillWindWheel");
-		
+			AttackWindWheel ();
+		if (Input.GetKeyDown (KeyCode.Q))
+		if ( !CheckAttacking() )
+			ChangeAttacking ( true );
+		else
+			ChangeAttacking ( false );
+		if (_attackTime > 0) {
+			_attackTime -= Time.deltaTime;
+		}
 	}
 
 	//==========================================================
@@ -43,7 +50,51 @@ public class PlayerAnimationControll : MonoBehaviour {
 	}
 
 
+	//--攻撃（AttackSingle）のアニメーションをする関数
+	public void AttackSingle() {
+		if (_attackTime > 0) return;
+		_animator.SetTrigger ("Attack");
+		_attackTime = ATTACK_INTERVAL;
+	}
 
+
+	//--攻撃（attackDouble）のアニメーションをする関数
+	public void AttackDouble() {
+		_animator.SetTrigger ("attackDouble");
+	}
+
+
+	//--攻撃（skillWindWheel）のアニメーションをする関数
+	public void AttackWindWheel() {
+		_animator.SetTrigger ("skillWindWheel");
+	}
+
+
+	//--死亡アニメーションをする関数
+	public void Death() {
+		_animator.SetTrigger ("death");
+	}
+
+
+	//--攻撃待機状態のオンオフをする関数
+	public void ChangeAttacking( bool x )
+	{
+		_animator.SetBool ("attackingWaiting", x);
+	}
+
+
+	//--攻撃待機状態かどうかを確認する関数
+	public bool CheckAttacking( )
+	{
+		return _animator.GetBool ("attackingWaiting");
+	}
+
+
+	//--nameのアニメーションが再生中かどうかを返す関数
+	public bool IsPlaying( string name ) {
+		AnimatorClipInfo[] animatorClipInfo = _animator.GetCurrentAnimatorClipInfo (0);	//どうやらanimatorClipInfoの参照を渡すように配列にしているだけで要素数は1のみみたい。
+		return animatorClipInfo[0].clip.name == name;
+	}
 	//==========================================================
 	//==========================================================
 }
