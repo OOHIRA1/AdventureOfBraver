22e261e7ef95bd68b31a37705245fae3fab0bc2a
oohira <t3e0vbbdwbd0zr6bgftu@docomo.ne.jp>
2018-05-05 12:20:45 +0900

    リファクタリング
    ・カメラのスクリプトをリファクタリングしました

--- Adventure of Braver/Assets/Scripts/PlayerScripts/TPSCamera1.cs
+++ Adventure of Braver/Assets/Scripts/PlayerScripts/TPSCamera1.cs
@@ -6,72 +6,70 @@ using UnityEngine;
 //
 //使用方法：TPSカメラワークをするカメラにアタッチ
 public class TPSCamera1 : MonoBehaviour {
-	[SerializeField] GameObject _target = null;
+	const float PI = 3.14f;
+	const float ANGLE_PER_SECOND = PI / 3f;								//角速度[rad / second]
+
+	[SerializeField] Transform _targetTransform = null;
 	[SerializeField] Vector3 _firstTToCPoint = new Vector3(0, 0.3f, -3f);	//_targetから見たカメラ(TargetToCamera)の初期座標
-	[SerializeField] float _firstDistance = 0;									//_targetのy軸との距離
-	[SerializeField] Vector3 _currentVelocity = Vector3.zero;
-	[SerializeField] Vector3 _tToCPoint = Vector3.zero;
-	public float _smoothTime = 0.1f;
-	public GameObject _z;
-	float a;
+	Vector3 _tToCPoint = Vector3.zero;										//_targetから見たカメラ(TargetToCamera)の座標
+	float _angle;															//_tToCPointのzx平面への正射影ベクトルのなす角
+	//[SerializeField] float _firstDistance = 0;								//_targetのy軸との距離
+	//[SerializeField] Vector3 _currentVelocity = Vector3.zero;				//カメラゆっくり近づく速度
+	//[SerializeField] float _smoothTime = 0.1f;							//カメラが近づき終わる時間
 
 
 	// Use this for initialization
 	void Start () {
-		transform.position = _target.transform.position + _firstTToCPoint;
-		Vector2 targetVec = new Vector2 ( _target.transform.position.x, _target.transform.position.z );
-		Vector2 cameraVec = new Vector2 (transform.position.x, transform.position.z);
-		_firstDistance = Vector2.Distance ( targetVec, cameraVec );
+		transform.position = _targetTransform.position + _firstTToCPoint;
+		//Vector2 targetVec = new Vector2 ( _target.transform.position.x, _target.transform.position.z );
+		//Vector2 cameraVec = new Vector2 (transform.position.x, transform.position.z);
+		//_firstDistance = Vector2.Distance ( targetVec, cameraVec );
 		_tToCPoint = _firstTToCPoint;
-		Vector3 point = new Vector3 ( _target.transform.position.x, transform.position.y, _target.transform.position.z );
-		Vector3 line = new Vector3 (transform.position.x - point.x, 0, transform.position.z - point.z);
-		a = -Vector3.Angle ( Vector3.right, line );
+		_angle = -Vector3.Angle ( Vector3.right, Vector3.ProjectOnPlane(_tToCPoint, Vector3.up) );
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		float distance = _z.transform.position.magnitude;
-		_z.transform.position = new Vector3 ( distance*Mathf.Cos(a), _z.transform.position.y, distance*Mathf.Sin(a) );
-		//a+= 0.1f;
-		Debug.Log(a);
 	}
 
 
 	void LateUpdate() {
-		//transform.LookAt ( _target.transform.position );	//こいつを使うとブレます！！
-		if (Vector3.Distance( _target.transform.position + _tToCPoint,  transform.position ) > 0f) {
-			//transform.position = Vector3.SmoothDamp (transform.position, _target.transform.position + _tToCPoint, ref _currentVelocity, _smoothTime);
-			transform.position = _target.transform.position + _tToCPoint;
+		//カメラを追従する処理-------------------------------------------------------------------------------------
+		if (_targetTransform.position + _tToCPoint - transform.position != Vector3.zero) {
+			transform.position = _targetTransform.position + _tToCPoint;
+			//transform.position = Vector3.SmoothDamp (transform.position, _target.transform.position + _tToCPoint, ref _currentVelocity, _smoothTime);	//ゆっくりカメラが近づく処理
 		}
-		Vector3 point = new Vector3 ( _target.transform.position.x, transform.position.y, _target.transform.position.z );
-		Vector2 line = new Vector2 (transform.position.x - point.x, transform.position.z - point.z);
-		float anglePerflame = 3.14f / 180f;
+		//--------------------------------------------------------------------------------------------------------
+
+		//カメラをターゲットを向きながら円運動させる処理----------------------------------------------------------------------------------------------
 		if (Input.GetAxis ("CameraHorizontal") != 0) {
-			if (Input.GetAxis ("CameraHorizontal") > 0) {
-				a += anglePerflame;
-			} else {
-				a -= anglePerflame;
-			}
+			//回転--------------------------------------------------------------------------------------------------------------------------
+			Vector3 centerOfRotation = new Vector3 ( _targetTransform.position.x, transform.position.y, _targetTransform.position.z );	//回転の中心
+			Vector3 rotationVector = Vector3.ProjectOnPlane (_tToCPoint, Vector3.up);	//回転するベクトル
+			_angle += ( ANGLE_PER_SECOND * Input.GetAxis ("CameraHorizontal") ) * Time.deltaTime;
+			rotationVector = new Vector3 (rotationVector.magnitude * Mathf.Cos (_angle), 0, rotationVector.magnitude * Mathf.Sin (_angle));
+			transform.position = centerOfRotation + rotationVector;
+			//transform.position = Vector3.MoveTowards( transform.position,  point + rotationVector, 1f );	//これでも出来る
+			//------------------------------------------------------------------------------------------------------------------------------
 
-			Vector3 movedLine = new Vector3 (line.magnitude * Mathf.Cos (a), 0, line.magnitude * Mathf.Sin (a));
-			transform.position = point + movedLine;
-			//transform.position = Vector3.MoveTowards( transform.position,  point + movedLine, 1f );
-			transform.forward = Vector3.RotateTowards (transform.forward, -movedLine.normalized, Vector3.Angle(transform.forward, -movedLine), 0);
-			_tToCPoint = transform.position - _target.transform.position;
+			//カメラをターゲットに向かせる----------------------------------------------------------------------------------------------------
+			transform.forward = Vector3.RotateTowards (transform.forward, -rotationVector.normalized, Vector3.Angle(transform.forward, -rotationVector), 0);
+			//transform.LookAt ( _target.transform.position );	//こいつを使うとブレます！！
+			//transform.RotateAround (centerOfRotation, Vector3.up, Input.GetAxis ("CameraHorizontal"));	//こいつを使うと回転の中心が動いた時の挙動がおかしい！
+			//-------------------------------------------------------------------------------------------------------------------------------
 
-			//transform.RotateAround (point, Vector3.up, Input.GetAxis ("CameraHorizontal"));	//こいつを使うと回転軸が動いた時の挙動がおかしいです！
-			if (Input.GetAxis ("Horizontal") == 0 && Input.GetAxis ("Vertical") == 0) {
-				_tToCPoint = transform.position - _target.transform.position;
-			}
+			_tToCPoint = transform.position - _targetTransform.position;	//_tToCPointの更新
 		}
+		//------------------------------------------------------------------------------------------------------------------------------------------
+
 		//カメラの位置を初期位置に戻す処理(未完成)----------------------------------------------------------------------------------------------------
 		if (Input.GetAxis ("CameraCancel") != 0) {
-			_tToCPoint = _target.transform.InverseTransformPoint( _target.transform.position + _firstTToCPoint);
-			transform.position = _target.transform.position + _tToCPoint;
-			Vector3 point2 = new Vector3 ( _target.transform.position.x, transform.position.y, _target.transform.position.z );
-			Vector3 line2 = new Vector3 (transform.position.x - point.x, 0, transform.position.z - point.z);
+			_tToCPoint = _targetTransform.InverseTransformPoint( _targetTransform.position + _firstTToCPoint);
+			transform.position = _targetTransform.position + _tToCPoint;
+			Vector3 point2 = new Vector3 ( _targetTransform.position.x, transform.position.y, _targetTransform.position.z );
+			Vector3 line2 = new Vector3 (transform.position.x - point2.x, 0, transform.position.z - point2.z);
 			transform.forward = Vector3.RotateTowards (transform.forward, -line2.normalized, Vector3.Angle(transform.forward, -line2), 0);
-			a = -Vector3.Angle ( Vector3.right, line );
+			_angle = -Vector3.Angle ( Vector3.right, line2 );
 		}
 		//------------------------------------------------------------------------------------------------------------------------------------
 	}
